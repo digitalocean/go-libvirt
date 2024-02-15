f7ba2d5342b1a7bc3ac3716e44edd9f534aaf0f7
Mike Pontillo <mpontillo@digitalocean.com>
2024-02-14 19:37:31 -0800

    c-for-go: use digitalocean-labs fork directly

--- scripts/install-c-for-go.sh
+++ scripts/install-c-for-go.sh
@@ -5,4 +5,4 @@ set -euo pipefail
 cd $(dirname $0)/..
 go get github.com/xlab/c-for-go
 go mod download
-GOBIN=$(pwd)/bin go install github.com/xlab/c-for-go
+GOBIN=$(pwd)/bin go install github.com/digitalocean-labs/c-for-go@latest
