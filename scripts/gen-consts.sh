3105246c40498943d92859be887086f070db2f2b
Mike Pontillo <mpontillo@digitalocean.com>
2024-02-27 15:04:21 -0800

    gen-consts.sh: ensure consistent tool versioning

--- scripts/gen-consts.sh
+++ scripts/gen-consts.sh
@@ -3,31 +3,33 @@
 # This runs the first code generator used by go-libvirt: c-for-go. This script
 # is run from the 'go generate ./...' command, and only needs to be run when
 # changing to a different version of libvirt.
+
+# Set TOP to the root of this repo, and SCRIPTS to the absolute path to the 
+# scripts/ directory.
+TOP="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
+SCRIPTS="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 if [ -z "${LIBVIRT_SOURCE}" ]; then
     echo "Set LIBVIRT_SOURCE to the root of the libvirt sources you want to use first."
     exit 1
 fi
+if [ -z "$GOBIN" ]; then
+    export GOBIN=$TOP/bin
+fi
 
 # Make sure c-for-go is installed
-if [ ! -x bin/c-for-go ] ; then
-    echo "c-for-go not found. Attempting to install it..."
-    if ! scripts/install-c-for-go.sh ; then
-        echo "failed to install c-for-go."
-        exit 1
-    fi
+echo "Attempting to install c-for-go..."
+if ! go install github.com/xlab/c-for-go@v1.1 ; then
+    echo "failed to install c-for-go."
+    exit 1
 fi
 
 # Make sure goyacc is installed (needed for the lvgen/ generator)
-if ! which goyacc > /dev/null; then
-    echo "goyacc not found. Attempting to install it..."
-    if ! go get golang.org/x/tools/cmd/goyacc/...; then
-        echo "failed to install goyacc. Please install it manually from https://golang.org/x/tools/cmd/goyacc"
-        exit 1
-    fi
+echo "Attempting to install goyacc..."
+if ! go install golang.org/x/tools/cmd/goyacc@v0.18.0; then
+    echo "failed to install goyacc. Please install it manually from https://golang.org/x/tools/cmd/goyacc"
+    exit 1
 fi
-
-# Set DIR to the absolute path to the scripts/ directory.
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # Ensure fresh output is in libvirt/
 rm -rf libvirt/
@@ -36,9 +38,9 @@ rm -rf libvirt/
 # lacks a mechanism for us to pass it a search path for header files.
 LVDIR=lv_source
 ln -sF ${LIBVIRT_SOURCE} ${LVDIR}
-if ! bin/c-for-go -nostamp -nocgo -ccincl libvirt.yml; then
+if ! c-for-go -nostamp -nocgo -ccincl libvirt.yml; then
     echo "c-for-go failed"
     exit 1
 fi
-mv libvirt/const.go ${DIR}/../const.gen.go
+mv libvirt/const.go ${SCRIPTS}/../const.gen.go
 rm ${LVDIR}
